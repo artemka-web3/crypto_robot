0200adcb5eb493b93f6c61d4c9872b042a5a83a5
arklual <arklual1@gmail.com>
2023-10-27 18:51:53 +0300

    upgrade analytics (fix json file)

--- update_table.py
+++ update_table.py
@@ -4,9 +4,10 @@ from analytics import update_sheet
 
 
 def schedule_tasks():
-    aioschedule.every(60).minutes.do(update_sheet())
+    aioschedule.every(60).minutes.do(update_sheet)
 
 async def main():
+    await update_sheet()
     schedule_tasks()
     while True:
         await aioschedule.run_pending()
